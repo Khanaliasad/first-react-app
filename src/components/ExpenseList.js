be3bf1455e4d35c6eadcbc562833e8b42a2a1327
Asad Ali Khan <khanaliasad.aak@gmail.com>
2021-10-29 04:48:59 +0500

    date component styling
    improve css and add flex sizeand Padding in % and give margin so the boxes can be spaced
    also added border to the date component

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,15 +1,15 @@
 import "./Expense.css";
 function Expense(props) {
-    const month = props.date.toLocaleString("en-us",{month:'long'});
-    const day = props.date.toLocaleString("en-us",{day:'2-digit'});
-    const year = props.date.getFullYear();
+  const month = props.date.toLocaleString("en-us", { month: "long" });
+  const day = props.date.toLocaleString("en-us", { day: "2-digit" });
+  const year = props.date.getFullYear();
 
   return (
     <div className="expense-item">
-      <div>
-          <div>{month}</div>
-          <div>{year}</div>
-          <div>{day}</div>
+      <div className="expece-date">
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
       </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
