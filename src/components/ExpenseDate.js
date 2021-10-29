b56d2279d382df00a38421bf27385a0eed1d0244
Asad Ali Khan <khanaliasad.aak@gmail.com>
2021-10-30 02:08:06 +0500

    few final adjustments to module 3 components

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -9,10 +9,10 @@ function ExpenseDate(props) {
 
   return (
     <div>
-      <div className="expece-date">
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
+      <div className="expense-date">
+        <div className='expense-date__month'>{month}</div>
+        <div className='expense-date__year'>{year}</div>
+        <div className='expense-date__day'>{day}</div>
       </div>
     </div>
   );
