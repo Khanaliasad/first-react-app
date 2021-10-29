01f882f9f86922a0087434e50d4ec91bac7c618c
Asad Ali Khan <khanaliasad.aak@gmail.com>
2021-10-29 05:25:50 +0500

    Splinted date component
    added new functional components of css and js named ExpenseDate

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,21 @@
+import "./ExpenseDate.css";
+
+import PropTypes from "prop-types";
+
+function ExpenseDate(props) {
+  const month = props.date.toLocaleString("en-us", { month: "long" });
+  const day = props.date.toLocaleString("en-us", { day: "2-digit" });
+  const year = props.date.getFullYear();
+
+  return (
+    <div>
+      <div className="expece-date">
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,16 +1,13 @@
 import "./Expense.css";
+import ExpenseDate from './ExpenseDate'
+
 function Expense(props) {
-  const month = props.date.toLocaleString("en-us", { month: "long" });
-  const day = props.date.toLocaleString("en-us", { day: "2-digit" });
-  const year = props.date.getFullYear();
+ 
+
 
   return (
     <div className="expense-item">
-      <div className="expece-date">
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
