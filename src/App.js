204913279cb070a68d6d7bac071e6afbc8208a5b
Asad Ali Khan <khanaliasad.aak@gmail.com>
2021-11-06 06:52:19 +0500

    minor changes comment made in app.js
    <br/> Update!!<br/> data is dynamic but runned into an error while updating array of dummy text now known as dummy text

--- src/App.js
+++ src/App.js
@@ -1,58 +1,59 @@
-import "./App.css";
-import Card from "./components/Card";
-// import React, {useState } from 'react'
-
 import Expense from "./components/Expense";
 import NewExpense from "./components/NewExpence/NewExpense";
 //import Expense from "./components/Expense";
+import "./App.css";
+import Card from "./components/Card";
+import React, { useState } from "react";
+
+
 //import Header from "./components/Header";
+
+let Dummy_Data = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: "e2",
+    title: "New TV",
+    amount: 799.49,
+    date: new Date(2021, 2, 12),
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+]
 
 function App() {
   let symb = ">";
-  let expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    {
-      id: "e2",
-      title: "New TV",
-      amount: 799.49,
-      date: new Date(2021, 2, 12),
-    },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
-
-   
+  //const [Expense, setExpense] = useState(Dummy_Data);
   const formExpenceDataHandeler = (data) => {
-    const ExpenseDataFromForm = { ...data }
-    console.log(data)
-   };
-
+    const ExpenseDataFromForm = { ...data };
+    // setExpense(data)
+  };
   return (
     <div className="App">
       <div>
         <NewExpense formExpenceData={formExpenceDataHandeler} />
         <Card>
-          <Expense item={expenses} />
+          <Expense item={Dummy_Data} />
           <div className="notes">
             <p>
               on 4th module vid 15 <br></br>
               idk what im doing next topic is map because for loop does not work
               = {symb} fuck u jsx
+              <br/> Update!!<br/> data is dynamic but runned into an error while updating array of dummy text now known as dummy text 
             </p>
           </div>
         </Card>
--- src/components/Expense.js
+++ src/components/Expense.js
@@ -3,24 +3,6 @@ import Card from "./Card";
 import ExpenseList from "./ExpenseList";
 function Expense(props) {
   let expenses = props.item;
-
-  //  let objlen = Object.keys(props.item).length;
-  // const asad = () => {
-  //   for (let i = 0; i < objlen; i++) {
-  //     console.log(props.item[i]);
-  //     let expenseitem = (
-  //       <div className="expense-item">
-  //         <ExpenseDate date={props.item[i].date} />
-  //         <div className="expense-item__description">
-  //           <h2>{props.item[i].title}</h2>
-  //           <div className="expense-item__price">${props.item[i].amount}</div>
-  //         </div>
-  //       </div>
-  //     );
-  //     return expenseitem;
-  //   }
-  // };
-
   return (
     <Card>
       {expenses.map((expense) => (
