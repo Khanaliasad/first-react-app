3dae5808e61b2399bbead3994bc141ca0ad0db0a
Asad Ali Khan <khanaliasad.aak@gmail.com>
2021-11-07 00:02:29 +0500

    made the filter using <select> for  years

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,10 +1,21 @@
 import "./Expense.css";
 import Card from "./Card";
 import ExpenseList from "./ExpenseList";
+import ExpenseFilter from './Filter/ExpenseFilter'
+import React, {useState} from 'react'
+
+
 function Expense(props) {
+  const [FilteredYear, setFilteredYear] = useState(2020)
   let expenses = props.item;
+  const ExpenseFilterHandeler = (event) => {
+    // console.log('filter event detected' + event)
+    setFilteredYear(event)
+  } 
+
   return (
     <Card>
+      <ExpenseFilter select={FilteredYear} onChangeFilter={ExpenseFilterHandeler} />
       {expenses.map((expense) => (
         <ExpenseList
           title={expense.title}
---
+++ src/components/Filter/ExpenseFilter.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Card from "../Card";
+import './ExpenseFilter.css'
+function ExpenseFilter(props) {
+  const DropdownHandeler = (event) => {
+    props.onChangeFilter(event.target.value);
+  };
+
+  return (
+    <Card className=''>
+      <div className="expenses-filter expenses-filter__all" >
+        <div className="expenses-filter__control">
+          <label>filter by year</label>
+          <select value={props.select} onChange={DropdownHandeler}>
+            <option value="2020">2020</option>
+            <option value="2021">2021</option>
+            <option value="2022">2022</option>
+            <option value="2023">2023</option>
+          </select>
+        </div>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseFilter;
